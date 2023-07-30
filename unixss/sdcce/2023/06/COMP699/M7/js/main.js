a42b8901549676720cc6a17eb2d138a834381869
Larry Breyer <larrybreyer@gmail.com>
2023-07-30 09:31:21 -0700

    daily tweaks

--- unixss/sdcce/2023/06/COMP699/M7/js/main.js
+++ unixss/sdcce/2023/06/COMP699/M7/js/main.js
@@ -2,7 +2,7 @@
 // HANDLE THE LOAD EVENT OF THE WINDOW
 
 // SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
-let w = 800;
+let w = 900;
 let h = 700;
 
 // RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
@@ -10,8 +10,9 @@ let h = 700;
 let l = (screen.availWidth -w) / 2;
 let t = (screen.availHeight -h) / 2;
 // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
-let pos = 'width='+w+',height='+h+ ',top='+t+',left='+l;
-alert(pos)
+// let pos = 'width='+w+',height='+h+ ',top='+t+',left='+l;
+let pos = 'width='+w+',height='+h;
+
 const addEmployee = () => {
   const w = window.open('add-employee.html','addEmployee',pos);
 }
