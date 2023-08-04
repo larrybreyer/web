8c11e36480026a6b2d842be231db2552c7dbec18
Larry Breyer <larrybreyer@gmail.com>
2023-08-03 18:22:42 -0700

    daily tweaks

--- unixss/sdcce/2023/06/COMP699/M8/js/script.js
+++ unixss/sdcce/2023/06/COMP699/M8/js/script.js
@@ -1,34 +1,64 @@
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
 
-// SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
-let count = 0;
-
 const $ = selector => document.querySelector(selector);
 
+// SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
+var count = 0;
 
 // ADD EMPLOYEE
-form.addEventListener('submit', (e) => {
+document.addEventListener('submit', (e) => {
+
     // PREVENT FORM SUBMISSION
-    form.submit.disabled = true;
+    e.target.disabled = true;
+    e.preventDefault();
 
 
     // GET THE VALUES FROM THE TEXT BOXES
-    let empData = 'ID: '+$("#id").value+'<br>Name:'+$("#name").value+'<br>Extension: '+$("#extension").value+'<br>Email: '+$("#email").value+'<br>Department: '+$("#department").value;
+    var id = $("#id").value;
+    var name = $("#name").value;
+    var ext = $("#extension").value;
+    var email = $("#email").value;
+    var dept = $("#department").value;
 
 
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+    //var row = $('#employees').insertRow(count);
+    var table = document.getElementById('employees');
+    var row   = table.insertRow(1);
+
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+    var cellId = row.insertCell(0);
+    var cellName = row.insertCell(1);
+    var cellExt = row.insertCell(2);
+    var cellEmail = row.insertCell(3);
+    var cellDept = row.insertCell(4);
+    var cellDelete = row.insertCell(5);
 
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
+    cellId.innerText = id;
+    cellName.innerText = name;
+    cellExt.innerText = ext;
+    cellEmail.innerText = email;
+    cellDept.innerText = dept;
+    cellDelete.innerHTML = '&#X274C';
 
+    // alert('stopping here.')
     // CREATE THE DELETE BUTTON
 
     // RESET THE FORM
+    $('#id').value = '';
+    $('#name').value = '';
+    $('#extension').value = '';
+    $('#email').value = '';
+//  $('#department').value = 'Administrative';
 
     // SET FOCUS BACK TO THE ID TEXT BOX
+    $('#id').focus();
 
     // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
+    count += 1;
+    $('#empCount').value = '('+count+')';
 
 });
 
