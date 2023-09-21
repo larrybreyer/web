af6ec286c03f58d1f5e7cad5706534d0a9c053af
Larry Breyer <larrybreyer@gmail.com>
2023-09-20 19:07:01 -0700

    daily tweaks

---
+++ unixss/sdcce/2023/09/COMP690/M02/Assignment01/js/script.js
@@ -0,0 +1,61 @@
+// STEP 1
+//  let someMonth
+//  function theMonth()
+//  let currentMonth
+//  let summerMonth
+//  let myLibraryFunction
+//
+// STEP 2 
+//  3.6
+//  'Hello!'
+//  false
+//  null
+//
+// STEP 3
+//  let anExpression = 3 * (4 / 5) + 6
+//
+// STEP 4
+//
+//  let firstName
+//  let lastName
+//  let address
+//  let city
+//  let state
+//  let zipCode
+//  let yourAge
+//  let referralSource
+//  let mayWeContactYou
+// 
+//  let strFirstName
+//  let strLastName
+//  let strAddress
+//  let strCity
+//  let strState
+//  let intZipCode
+//  let intYourAge
+//  let strReferralSource
+//  let blnMayWeContactYou
+//
+// STEP 5
+//  let address = '10 Downing Street'
+//  let zipCode = 92108
+//  let mayWeContactYou = false
+//
+// STEP 6
+	console.log(10 + ' Downing')
+// STEP 7
+// STEP 8
+// STEP 9
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 10
+// STEP 20
+// STEP 20
+// STEP 20
